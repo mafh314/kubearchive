f38de7761bc2c5a9937b4d8c01681ddfee3c8d5a
M. Andrea Fernández <manferna@redhat.com>
2024-11-20 19:02:18 +0100

    Set default timezone in postgreSQL
    Signed-off-by: M. Andrea Fernández <manferna@redhat.com>

--- integrations/database/postgresql/kubearchive.sql
+++ integrations/database/postgresql/kubearchive.sql
@@ -42,6 +42,27 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Name: kubearchive; Type: DATABASE PROPERTIES; Schema: -; Owner: kubearchive
+--
+
+ALTER DATABASE kubearchive SET "TimeZone" TO 'UTC';
+
+
+\connect kubearchive
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
 -- Name: trigger_set_timestamp(); Type: FUNCTION; Schema: public; Owner: kubearchive
 --
 
@@ -68,7 +89,10 @@ SET default_table_access_method = heap;
 CREATE TABLE public.log_url (
     id bigint NOT NULL,
     uuid uuid NOT NULL,
-    url text NOT NULL
+    url text NOT NULL,
+    container_name text NOT NULL,
+    created_at timestamp without time zone DEFAULT now() NOT NULL,
+    updated_at timestamp without time zone DEFAULT now() NOT NULL
 );
 
 
@@ -225,6 +249,13 @@ CREATE INDEX resource_kind_namespace_idx ON public.resource USING btree (kind, a
 
 
 --
+-- Name: log_url set_timestamp; Type: TRIGGER; Schema: public; Owner: kubearchive
+--
+
+CREATE TRIGGER set_timestamp BEFORE UPDATE ON public.log_url FOR EACH ROW EXECUTE FUNCTION public.trigger_set_timestamp();
+
+
+--
 -- Name: resource set_timestamp; Type: TRIGGER; Schema: public; Owner: kubearchive
 --
 
@@ -242,3 +273,4 @@ ALTER TABLE ONLY public.log_url
 --
 -- PostgreSQL database dump complete
 --
+
